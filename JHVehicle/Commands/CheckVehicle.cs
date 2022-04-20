c396cdd14517fc78a9a93c44d00777c6770e9e27
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2022-04-20 22:18:13 +0200

    Add files via upload

---
+++ JHVehicle/Commands/CheckVehicle.cs
@@ -0,0 +1,34 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+
+namespace JHVehicle.Commands
+{
+    public class CheckVehicle : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public System.Collections.Generic.List<string> Permissions
+        {
+            get
+            {
+                return new System.Collections.Generic.List<string>() {
+                    "jhvehicles.checkvehicle"
+                };
+            }
+        }
+        public string Name = "checkvehicle";
+        public string Help => "Check your vehicles speed.";
+        public string Syntax => "";
+        public System.Collections.Generic.List<string> Aliases => new System.Collections.Generic.List<string> { "vehiclespeed", "vspeed", "checkvehiclespeed" };
+        string IRocketCommand.Name => "checkvehicle";
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer Player = (UnturnedPlayer)caller;
+            if (Player.IsInVehicle)
+            {
+                InteractableVehicle vehicle = Player.CurrentVehicle;
+                Rocket.Unturned.Chat.UnturnedChat.Say(Player, "Speed: " + vehicle.speed + ", " + vehicle.physicsSpeed + ", " + vehicle.spedometer + " kph");
+            }
+        }
+    }
+}
---
+++ JHVehicle/Commands/DmgMultiplier.cs
@@ -0,0 +1,43 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Player;
+
+namespace JHVehicle.Commands
+{
+    public class DmgMultiplier : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+        public System.Collections.Generic.List<string> Permissions
+        {
+            get
+            {
+                return new System.Collections.Generic.List<string>() {
+                    "jhvehicles.multiplier"
+                };
+            }
+        }
+        public string Name = "dmgmultiplier";
+        public string Help => "Change the multiplier for damage from vehicles speeds.";
+        public string Syntax => "";
+        public System.Collections.Generic.List<string> Aliases => new System.Collections.Generic.List<string> { "vehicledamagemultiplier", "vehicledmgmultiplier", "vdmg", "vdm" };
+        string IRocketCommand.Name => "dmgmultiplier";
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 0)
+            {
+                if (command[0] == "save")
+                {
+                    JHVehicle.Instance.Configuration.Save();
+                    Rocket.Unturned.Chat.UnturnedChat.Say(caller, "Saved!");
+                }
+                else
+                {
+                    float.TryParse(command[0], out JHVehicle.Instance.Configuration.Instance.Multiplier);
+                    Rocket.Unturned.Chat.UnturnedChat.Say(caller, "Updated damage multiplier to: " + JHVehicle.Instance.Configuration.Instance.Multiplier);
+                }
+            }else
+            {
+                Rocket.Unturned.Chat.UnturnedChat.Say(caller, "<multiplier, number>, currently: "+ JHVehicle.Instance.Configuration.Instance.Multiplier);
+            }
+        }
+    }
+}
